23b4cd8799a0a9e1a6d7c6b3000990b30b78742e
Aitugan <mirash2001@gmail.com>
2024-11-07 06:14:58 +0500

    hw1 upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Assignments for KBTU CSCI 3241: Reinforcement Learning.
---
+++ hw1/setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='csci3241',
+    version='0.1.0',
+    packages=['csci3241'],
+)
